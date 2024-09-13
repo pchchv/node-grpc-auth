2f531755a739bbb0a58d19ced7b38d48a03097ae
pchchv <ipchchv@gmail.com>
2024-09-13 22:46:37 +0300

    Implement a login

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,39 @@
+import {
+    Server,
+    sendUnaryData,
+    ServerUnaryCall,
+} from "@grpc/grpc-js";
+import {
+    LoginCode,
+    LoginRequest,
+    LoginResult,
+} from "../../protos/auth";
+  
 const users = [{
     id: 0,
     username: "admin",
     password: "adm1n"
 }];
+
+const login = (
+    call: ServerUnaryCall<LoginRequest, LoginResult>,
+    callback: sendUnaryData<LoginResult>
+) => {
+    const requestData = call.request;
+    const user = users.find(
+        (user) => user.username === requestData.username && user.password === requestData.password
+    );
+
+    if (user) {
+        const result: LoginResult = {
+            loginCode: LoginCode.SUCCESS,
+            token: "RandomSecretToken",
+        };
+        callback(null, result);
+    } else {
+        const result: LoginResult = {
+            loginCode: LoginCode.FAIL,
+        };
+        callback(null, result);
+    }
+};
