2768bcea30933260d475f68039df88e59ffd68d4
pchchv <ipchchv@gmail.com>
2024-09-13 23:00:12 +0300

    Lint

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,39 +1,43 @@
 import {
-    Server,
-    sendUnaryData,
-    ServerUnaryCall,
+  Server,
+  sendUnaryData,
+  ServerUnaryCall,
 } from "@grpc/grpc-js";
 import {
-    LoginCode,
-    LoginRequest,
-    LoginResult,
+  LoginCode,
+  LoginResult,
+  LoginRequest,
 } from "../../protos/auth";
-  
-const users = [{
+
+const users = [
+  {
     id: 0,
     username: "admin",
-    password: "adm1n"
-}];
+    password: "adm1n",
+  },
+];
 
 const login = (
-    call: ServerUnaryCall<LoginRequest, LoginResult>,
-    callback: sendUnaryData<LoginResult>
+  call: ServerUnaryCall<LoginRequest, LoginResult>,
+  callback: sendUnaryData<LoginResult>,
 ) => {
-    const requestData = call.request;
-    const user = users.find(
-        (user) => user.username === requestData.username && user.password === requestData.password
-    );
+  const requestData = call.request;
+  const user = users.find(
+    (user) =>
+      user.username === requestData.username &&
+      user.password === requestData.password,
+  );
 
-    if (user) {
-        const result: LoginResult = {
-            loginCode: LoginCode.SUCCESS,
-            token: "RandomSecretToken",
-        };
-        callback(null, result);
-    } else {
-        const result: LoginResult = {
-            loginCode: LoginCode.FAIL,
-        };
-        callback(null, result);
-    }
+  if (user) {
+    const result: LoginResult = {
+      loginCode: LoginCode.SUCCESS,
+      token: "RandomSecretToken",
+    };
+    callback(null, result);
+  } else {
+    const result: LoginResult = {
+      loginCode: LoginCode.FAIL,
+    };
+    callback(null, result);
+  }
 };
