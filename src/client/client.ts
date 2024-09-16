965af3edc4247b6f100cb36336b331728dcc942e
pchchv <ipchchv@gmail.com>
2024-09-16 15:00:05 +0300

    Implement a client

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,6 +1,12 @@
-import { LoginRequest } from "../../protos/auth";
+import { AuthServiceClient, LoginRequest } from "../../protos/auth";
+import { credentials } from "@grpc/grpc-js";
 
 const loginRequest: LoginRequest = {
   username: "admin",
   password: "qwerty",
 };
+
+const client = new AuthServiceClient(
+  "localhost:8080",
+  credentials.createInsecure()
+);
